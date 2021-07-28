14ec81f0b41d31a7f072dfb1c301805b6c8b6bb0
Donatas <donkuprys@gmail.com>
2021-07-28 19:23:01 +0300

    doneForNow

--- Plates.js
+++ Plates.js
@@ -7,20 +7,47 @@ class Plates {
 
     intro() {
         if (this.cleanPlateCount + this.dirtyPlateCount <= 0) {
-            console.log(`Hi, this restoran has 0 plates! Please, buy some.`);
+            console.log(`Hi, this restaurant has 0 plates! Please, buy some.`);
         } else {
-            console.log(`Hi, this restoran has ${this.cleanPlateCount + this.dirtyPlateCount} plates!`);
+            console.log(`Hi, this restaurant has ${this.cleanPlateCount + this.dirtyPlateCount} plates!`);
         }
     }
     buyPlates(platesCount) {
-        this.cleanPlateCount = platesCount;
-        console.log(`Restoran purchased ${platesCount} plates.`);
+        this.cleanPlateCount += platesCount;
+        console.log(`Restaurant purchased ${platesCount} plates.`);
 
     }
-    intro() {
-        console.log(`Hi, this restoran has ${this.cleanPlateCount + this.dirtyPlateCount} plates!`);
+    usePlates(usedPlates) {
+        if (usedPlates <= this.cleanPlateCount) {
+            this.cleanPlateCount -= usedPlates;
+            this.dirtyPlateCount += usedPlates;
+            console.log(`Restaurant has made some dishes for ${usedPlates} plates.`);
+        } else {
+            console.log(`Restaurant has ${this.cleanPlateCount} plates only, it's not enough. No action is taken.`);
+        }
     }
+    cleanPlatesCount() {
+        if (this.cleanPlateCount === 0) {
+            console.log(`Restaurant is left with no clean plates.`);
+        } else {
+            console.log(`Restaurant has ${this.cleanPlateCount} clean plates.`);
+        }
 
+    }
+    cleanUp(platesCount) {
+        if (platesCount > this.dirtyPlateCount) {
+            console.log(`Restaurant does not have so many dirty plates.`);
+            return;
+        }
+
+        this.dirtyPlateCount -= platesCount;
+        this.cleanPlateCount += platesCount;
+
+        if (this.dirtyPlateCount === 0) {
+            console.log(`All plates in the restaurant is clean!`);
+        }
+
+    }
 
 }
 
--- index.js
+++ index.js
@@ -1,42 +1,45 @@
 const Plates = require('./Plates');
 
-const restoran = new Plates('Jonas');
+const restaurant = new Plates('Jonas');
 
-restoran.intro();
-// Hi, this restoran has no plates! Please, buy some.
+restaurant.intro();
+// Hi, this restaurant has no plates! Please, buy some.
 
-restoran.buyPlates(50);
-// // Restoran purchased 50 plates.
+restaurant.buyPlates(50);
+// // restaurant purchased 50 plates.
 
-restoran.intro();
-// // Hi, this restoran has 50 plates!
 
-// restoran.usePlates(20);
-// // Restoran has made some dishes for 20 plates.
+restaurant.intro();
+// // Hi, this restaurant has 50 plates!
 
-// restoran.cleanPlatesCount();
-// // Restoran has 30 clean plates.
+restaurant.usePlates(20);
+// // restaurant has made some dishes for 20 plates.
 
-// restoran.usePlates(20);
-// // Restoran has made some dishes for 20 plates.
+restaurant.cleanPlatesCount();
+// // restaurant has 30 clean plates.
 
-// restoran.cleanPlatesCount();
-// // Restoran has 10 clean plates.
+restaurant.usePlates(20);
+// // restaurant has made some dishes for 20 plates.
 
-// restoran.usePlates(20);
-// // Restoran has 10 plates only, it's not enough. No action is taken.
+restaurant.cleanPlatesCount();
+// // restaurant has 10 clean plates.
 
-// restoran.cleanUp(60);
-// // Restoran does not have so many dirty plates.
+restaurant.usePlates(20);
+// // restaurant has 10 plates only, it's not enough. No action is taken.
 
-// restoran.buyPlates(10);
-// // Restoran purchased 10 plates.
+restaurant.cleanUp(60);
+// // restaurant does not have so many dirty plates.
 
-// restoran.usePlates(20);
-// // Restoran has made some dishes for 20 plates.
+restaurant.buyPlates(10);
+// // restaurant purchased 10 plates.
 
-// restoran.cleanPlatesCount();
-// // Restoran is left with no clean plates.
+restaurant.usePlates(20);
+// // restaurant has made some dishes for 20 plates.
 
-// restoran.cleanUp(60);
-// // All plates in the Restoran is clean!
+restaurant.cleanPlatesCount();
+// // restaurant is left with no clean plates.
+
+restaurant.cleanUp(60);
+// // All plates in the restaurant is clean!
+
+restaurant.intro();
